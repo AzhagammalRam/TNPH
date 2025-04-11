dceebe1ad526e2642d4d3e8733ea2c5f522f409d
sudhansmart <136149970+sudhansmart@users.noreply.github.com>
2025-04-11 15:24:36 +0530

    changed

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -26,41 +26,41 @@ const App = () => {
     <BrowserRouter basename='/'>
       <Routes>
         <Route path="/" element={<Home />} />
-          <Route path="/add-session" element={<AddSession />} />
-          <Route path="/classes-admin" element={<ClassAdmin />} />
-          <Route path="/course-year" element={<CourseYear />} />
-          <Route path="/create-notice" element={<CreateNotice />} />
-          <Route path="/edit-session" element={<EditSession />} />
-          <Route path="/group-session" element={<GroupSession />} />
-          <Route path="/index-admin" element={<IndexAdmin />} />
-          <Route path="/login-staff" element={<LoginAdmin />} />
-          <Route path="/notice-admin" element={<NoticeAdmin />} />
-          <Route path="/register-staff" element={<RegisterAdmin />} />
-          <Route path="/show-notice" element={<ShowNotices />} />
-          <Route path="/profile-admin" element={<ProfileAdmin />} />
-          <Route path="/class-admin" element={<Classes />} />
-          <Route path="/year-group" element={<YearGroup />} />
-          <Route path="/index-student" element={<IndexStudent />} />
-          <Route path="/login-student" element={<LoginStudent />} />
-          <Route path="/profile-student" element={<ProfileStudent/>} />
-          <Route path="/register-student" element={<RegisterStudent/>} />
-          <Route path="/week-classes" element={<WeekClasses/>} />
+        <Route path="/add-session" element={<AddSession />} />
+        <Route path="/classes-admin" element={<ClassAdmin />} />
+        <Route path="/course-year" element={<CourseYear />} />
+        <Route path="/create-notice" element={<CreateNotice />} />
+        <Route path="/edit-session" element={<EditSession />} />
+        <Route path="/group-session" element={<GroupSession />} />
+        <Route path="/index-admin" element={<IndexAdmin />} />
+        <Route path="/login-staff" element={<LoginAdmin />} />
+        <Route path="/notice-admin" element={<NoticeAdmin />} />
+        <Route path="/register-staff" element={<RegisterAdmin />} />
+        <Route path="/show-notice" element={<ShowNotices />} />
+        <Route path="/profile-admin" element={<ProfileAdmin />} />
+        <Route path="/class-admin" element={<Classes />} />
+        <Route path="/year-group" element={<YearGroup />} />
+        <Route path="/index-student" element={<IndexStudent />} />
+        <Route path="/login-student" element={<LoginStudent />} />
+        <Route path="/profile-student" element={<ProfileStudent />} />
+        <Route path="/register-student" element={<RegisterStudent />} />
+        <Route path="/week-classes" element={<WeekClasses />} />
 
-          <Route path="/index-computerProgrammer" element={<IndexComputerProgrammer />} />
-          <Route path="/DsrTrainingForm" element={<DsrTrainingForm />} />
-          <Route path="/EventsAndCeremonies" element={<EventsAndCeremonies />} />
-          <Route path="/EventsAndCeremoniesReport" element={<EventsAndCeremoniesReport />} />
-          <Route path="/Leave" element={<Leave />} />
-          <Route path="/LeaveReport" element={<LeaveReport />} />
-          <Route path="/Strength" element={<Strength />} />
-          <Route path="/StrengthReport" element={<StrengthReport />} />
-          <Route path="/TrainingDivision" element ={<TrainingDivision /> } />
-          <Route path="/index-SPCamp" element ={<IndexSPCamp /> } />
-          <Route path="/index-SP" element ={<IndexSP /> } />
-          <Route path="/index-Principal" element ={<IndexPrincipal /> } />
-          <Route path="/DsrReport" element ={<DsrReport /> } />
-          <Route path="/AddBatch" element ={<AddBatch /> } />
-          <Route path="/BatchDetails" element ={<BatchDetails /> } />
+        <Route path="/index-computerProgrammer" element={<IndexComputerProgrammer />} />
+        <Route path="/DsrTrainingForm" element={<DsrTrainingForm />} />
+        <Route path="/EventsAndCeremonies" element={<EventsAndCeremonies />} />
+        <Route path="/EventsAndCeremoniesReport" element={<EventsAndCeremoniesReport />} />
+        <Route path="/Leave" element={<Leave />} />
+        <Route path="/LeaveReport" element={<LeaveReport />} />
+        <Route path="/Strength" element={<Strength />} />
+        <Route path="/StrengthReport" element={<StrengthReport />} />
+        <Route path="/TrainingDivision" element={<TrainingDivision />} />
+        <Route path="/index-SPCamp" element={<IndexSPCamp />} />
+        <Route path="/index-SP" element={<IndexSP />} />
+        <Route path="/index-Principal" element={<IndexPrincipal />} />
+        <Route path="/DsrReport" element={<DsrReport />} />
+        <Route path="/AddBatch" element={<AddBatch />} />
+        <Route path="/BatchDetails" element={<BatchDetails />} />
       </Routes>
 
     </BrowserRouter>
